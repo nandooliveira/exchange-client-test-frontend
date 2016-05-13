6f675c3c93965ef63c5c72b8f3dff61a0b86fc9e
Fernando Oliveira <nandooliveira.al@gmail.com>
2016-05-13 01:03:44 -0300

    adding node to packages

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ app = express();
 
 app.use(express.static(__dirname));
 app.get('/', function (req, res) {
-	res.sendfile('index.html', {root: __dirname})
+	res.sendfile('app/index.html', {root: __dirname})
 });
 var server = app.listen(process.env.PORT || 80);

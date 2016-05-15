37251bdfc0796faa7eda1260eefc000b8f1af53d
Fernando Oliveira <nandooliveira.al@gmail.com>
2016-05-15 03:40:20 -0300

    Adding new chart

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -11,9 +11,12 @@ angular.module('myApp.dashboard', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope', 'exchangeRateService', function($scope, exchangeRateService) {
 
+  // Default period
   $scope.selectedItem = "today";
+  // Values to period select
   $scope.periodOptions = [{value: "today", label: "Today"}, {value: "week", label: "Week"}, {value: "month", label: "Month"}];
 
+  // Load current rates to show o top
   $scope.currentRates = new Array();
   exchangeRateService.getLast().then(
     function (data) {
@@ -21,55 +24,14 @@ angular.module('myApp.dashboard', ['ngRoute'])
     }
   );
 
-
+  // Load data to chart of quotation by period and initializes the chart
+  // when download finishs
   $scope.loadChart = function () {
 
     exchangeRateService.loadAll($scope.selectedItem).then(
       function (data) {
-
-        var labels = [];
-        var graphData = [];
-
-        var auxDate = "";
-        for (var i in data) {
-          if ($scope.selectedItem == "today") {
-            var newDate = data[i].datetime.slice(0, 16).replace("T", " ");
-          } else {
-            var newDate = data[i].datetime.slice(0, 10).replace("T", " ");
-          }
-
-          if (newDate != auxDate) {
-            labels.push(newDate);
-            graphData.push(data[i].rates[0].rate);
-
-            auxDate = newDate;
-          }
-        }
-
-        var ctx = $("#mainChart");
-        var myChart = new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: labels,
-            datasets: [{
-              label: '# commercial dolar along time',
-              data: graphData
-            }]
-          },
-          options: {
-            responsive: true,
-            showXLabels: 10,
-            skipXLabels: 5,
-            scales: {
-              yAxes: [{
-
-                ticks: {
-                  beginAtZero:true
-                }
-              }]
-            }
-          }
-        });
+        mountChartOfCommercialQuotationByPeriod(data);
+        loadComparativeChart(data);
       }, function (error) {
         alert('erro: ' + error);
       });
@@ -77,16 +39,134 @@ angular.module('myApp.dashboard', ['ngRoute'])
     $scope.loadChart();
 
 
+    function mountChartOfCommercialQuotationByPeriod(data) {
+      var labels = [];
+      var graphData = [];
+
+      var auxDate = "";
+      for (var i in data) {
+        var quant = $scope.selectedItem == "today" ? 16 : 10;
+        var newDate = data[i].datetime.slice(0, quant).replace("T", " ");
+
+        if (newDate != auxDate) {
+          labels.push(newDate);
+          graphData.push(data[i].rates[0].rate);
+          auxDate = newDate;
+        }
+      }
+
+      var ctx = $("#mainChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: '# commercial dolar along time',
+            data: graphData
+          }]
+        },
+        options: {
+          responsive: true,
+          showXLabels: 10,
+          skipXLabels: 5,
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero:true
+              }
+            }]
+          }
+        }
+      });
+    }
+
+    function loadComparativeChart (data) {
+      var labels = [];
+      var graphData = [];
+      var auxDate = "";
+      for (var i in data) {
+
+        var quotation = data[i];
+
+        var quant = $scope.selectedItem == "today" ? 16 : 10;
+        var newDate = quotation.datetime.slice(0, quant).replace("T", " ");
+
+        if (newDate != auxDate) {
+          labels.push(newDate);
+
+          for (var i in quotation.rates) {
+            var rate = quotation.rates[i];
+
+            if (!graphData[rate.vetProvider]) {
+              graphData[rate.vetProvider] = [];
+            }
+
+            graphData[rate.vetProvider].push(rate.rate);
+          }
+          auxDate = newDate;
+        }
+      }
+
+      var datasets = [];
+      var strokecolors = ['rgba(220,180,0,1)', 'rgba(235,19,66,1)', 'rgba(58,110,44,1)', 'rgba(148,52,148,1)', 'rgba(220,220,220,1)'];
+      var i = 0;
+      for (var key in graphData) {
+        datasets.push(
+          {
+            label: key,
+            data: graphData[key],
+            borderColor: strokecolors[i],
+            backgroundColor:
+            strokecolors[i],
+            fill:false
+          }
+        );
+        console.log(strokecolors[i]);
+        i++;
+      }
+
+      console.log(datasets);
+
+      var ctx = $("#comparativeChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: datasets
+        },
+        options: {
+          responsive: true,
+          showXLabels: 10,
+          skipXLabels: 5,
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero:true
+              }
+            }]
+          }
+        }
+      });
+    }
+
   }])
 
-
+  /**
+  Service to load exchange rates
+  */
   .factory('exchangeRateService', ['$q', '$http', function ($q, $http) {
     var BASE_URL = "http://floating-spire-61246.herokuapp.com/api/v1/exchange_rate";
     var factory = {};
+
+    /**
+    Load all exchange Rates
+    */
     factory.loadAll = function (selectedItem) {
       var result = $q.defer();
       var datetime = new Date();
       var url = BASE_URL;
+
+      // Mount URL depending on selectedItem
       if (selectedItem == null || selectedItem == "today") {
         var today = datetime.toISOString().slice(0,10)
         url += '?datetime=' + today;
@@ -105,21 +185,22 @@ angular.module('myApp.dashboard', ['ngRoute'])
         url += '/' + curr.toISOString().slice(0,8) + "01/" + curr.toISOString().slice(0,8) + "31";
       }
 
+      // Do request
       $http({
         method: 'GET',
         url: url
       }).then(function successCallback(response) {
         result.resolve(response.data);
       }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        console.log("error");
         console.log(response);
       });
 
       return result.promise;
     }
 
+    /**
+    Loads current quotation
+    */
     factory.getLast = function () {
       var result = $q.defer();
       $http({
@@ -128,7 +209,6 @@ angular.module('myApp.dashboard', ['ngRoute'])
       }).then(function successCallback(response) {
         result.resolve(response.data);
       }, function errorCallback(response) {
-        console.log("error");
         console.log(response);
       });
 
